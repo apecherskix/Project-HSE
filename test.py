0f8a2b334983814f6ee4a6a63f7a10f189da6884
apecherskix <116425810+apecherskix@users.noreply.github.com>
2022-12-15 21:25:24 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+from dude import Duude
+import unittest
+import random
+import time
+dude=Duude(A)
+class GameTest(unittest.TestCase):
+    def test_of_work(self):
+        dude.work()
+        self.assertTrue(dude.energy==50)
+    def test_of_sleep(self)
+        dude.sleep()
+                self.assertTrue(dude.energy==100)
+if __name__ == 'dude':
+    unittest.main()
